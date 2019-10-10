6b32f2846deffce62c536676a504e20e14be5374
junedacaya <54863402+junedacaya@users.noreply.github.com>
2019-10-10 14:31:15 -0400

    added readings screen

--- app/src/main/java/com/example/solarcontrollerproject/ui/readings/ReadingsFragment.java
+++ app/src/main/java/com/example/solarcontrollerproject/ui/readings/ReadingsFragment.java
@@ -1,4 +1,4 @@
-package com.example.solarcontrollerproject.ui.slideshow;
+package com.example.solarcontrollerproject.ui.readings;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.solarcontrollerproject.R;
 
-public class SlideshowFragment extends Fragment {
+public class ReadingsFragment extends Fragment {
 
-    private SlideshowViewModel slideshowViewModel;
+    private ReadingsViewModel readingsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
+        readingsViewModel =
+                ViewModelProviders.of(this).get(ReadingsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_readings, container, false);
+        final TextView textView = root.findViewById(R.id.text_readings);
+        readingsViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/solarcontrollerproject/ui/readings/ReadingsViewModel.java
+++ app/src/main/java/com/example/solarcontrollerproject/ui/readings/ReadingsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.solarcontrollerproject.ui.slideshow;
+package com.example.solarcontrollerproject.ui.readings;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class SlideshowViewModel extends ViewModel {
+public class ReadingsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public SlideshowViewModel() {
+    public ReadingsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        mText.setValue("Readings");
     }
 
     public LiveData<String> getText() {
